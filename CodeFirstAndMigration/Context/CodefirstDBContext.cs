ff9a065db4f87f565a2c7e36966cb48d05b257ca
faruk <faruk@DESKTOP-P7KA77K>
2024-02-22 16:07:51 +0300

    Add project files.

---
+++ CodeFirstAndMigration/Context/CodefirstDBContext.cs
@@ -0,0 +1,19 @@
+﻿using CodeFirstAndMigration.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CodeFirstAndMigration.Context
+{
+    public class CodefirstDBContext : DbContext //DbContext microsoftEf 'den geliyor
+    {
+
+        public DbSet<Customer> Customers { get; set; } //Tablolar
+        public DbSet<Product> Products { get; set; } //Tablolar
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-P7KA77K\\SQLEXPRESS;Database=CodefirstDB;User Id=sa;Password=1234; ;TrustServerCertificate=true");
+        }
+
+    }
+}
